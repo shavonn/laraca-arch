627ac9f60a2e78af69867ef9c6892bd32de7e78c
Shavonn Brown <sha@handsomebrown.com>
2023-12-20 13:04:44 -0500

    comment out example

--- tests/Pest.php
+++ tests/Pest.php
@@ -28,9 +28,9 @@ uses(LaracaTestCase::class)->in(__DIR__);
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+// expect()->extend('toBeOne', function () {
+//     return $this->toBe(1);
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -43,7 +43,7 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
-{
-    // ..
-}
+// function something()
+// {
+//     // ..
+// }
