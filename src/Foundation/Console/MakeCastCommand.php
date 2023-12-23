bbdf9583182c1dbf91edeb70c05852785f0d3238
Shavonn Brown <sha@handsomebrown.com>
2023-12-23 10:32:46 -0500

    lar-2 make:value (#15)
    * moving commands back to parent folder
    
    * make:value
    
    * fix value namespace
    
    * make:value fixes
    
    * test make:model uuid option

---
+++ src/Foundation/Console/MakeCastCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace HandsomeBrown\Laraca\Foundation\Console;
+
+use Illuminate\Foundation\Console\CastMakeCommand;
+
+class MakeCastCommand extends CastMakeCommand
+{
+    /**
+     * name
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:cast';
+
+    /**
+     * getDefaultNamespace
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace.'\\'.config('laraca.cast.namespace');
+    }
+}
